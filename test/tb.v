4891076188fd47a79f6c5ad70af0a14921cd6ba7
MoisesPabloc <epabloc@miumg.edu.gt>
2025-06-02 23:15:55 -0600

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,38 +1,38 @@
-`default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+module test_tb;
 
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
+    reg  [7:0] io_in;
+    reg        clk;
+    wire [15:0] io_out;
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+    tt_um_alu_prefix_display dut (
+        .io_in(io_in),
+        .io_out(io_out),
+        .clk(clk)
+    );
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+    initial clk = 0;
+    always #20 clk = ~clk;  // 25 MHz
+
+    initial begin
+        $display("Inicio del testbench");
+
+        io_in = 16'b00000000_00000000;
+        #100;
+
+        io_in[7:0]   = 8'b00000101; // A = 5
+        io_in[12:8]  = 5'b00011;    // B = 3
+        io_in[15:13] = 3'b000;
+        io_in[16]    = 1'b0;
+        #100;
+
+        io_in[7:0]   = 8'b00001010; // A = 10
+        io_in[12:8]  = 5'b00100;    // B = 4
+        io_in[15:13] = 3'b001;
+        #100;
+
+        $finish;
+    end
 
 endmodule
